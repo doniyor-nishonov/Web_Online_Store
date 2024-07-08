f3d19db846f6e58ce96d0fb62c1b70c6983ba5d7
Sardor <Sardor>
2024-07-08 16:22:22 +0500

    ...

--- src/main/java/com/pdp/web_online_store/servlets/LoginServlet.java
+++ src/main/java/com/pdp/web_online_store/servlets/LoginServlet.java
@@ -1,5 +1,8 @@
 package com.pdp.web_online_store.servlets;
 
+import com.pdp.web_online_store.entity.user.Users;
+import com.pdp.web_online_store.service.user.UserService;
+import com.pdp.web_online_store.service.user.UsersServiceImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,13 +14,22 @@ import java.io.IOException;
 @WebServlet(name = "login", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
 
+    private final UserService userService = new UsersServiceImpl();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("views/user/login.jsp").forward(req,resp);
+        req.getRequestDispatcher("views/user/login.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String email = req.getParameter("email");
+
+        String password = req.getParameter("password");
+
+        userService.login(email, password);
+
 
     }
 }
